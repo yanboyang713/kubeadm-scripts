7711b1145ee1d77818856ffc195907ff01c799de
Boyang Yan <yanboyang713@gmail.com>
2023-05-31 23:43:44 -0400

    disable swap

---
+++ scripts/disableSwap.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Function to comment out lines containing the keyword
+comment_lines_with_keyword() {
+    local keyword=$1
+    local file=$2
+
+    # Create a temporary file
+    local temp_file=$(mktemp)
+
+    while IFS= read -r line; do
+        if [[ $line == *"$keyword"* ]]; then
+            echo "# $line" >> "$temp_file"
+        else
+            echo "$line" >> "$temp_file"
+        fi
+    done < "$file"
+    # Make file backup
+    sudo mv "$file" "$file.backup"
+    # Overwrite the original file with the modified content
+    sudo mv "$temp_file" "$file"
+    echo "Modified content written back to $file"
+}
+
+# check swap size, if swap great than 1 bytes, disable Swap permanently
+disable_swap_permanently() {
+    # Get swap size in bytes
+    swap_size=$(grep 'SwapTotal' /proc/meminfo | awk '{print $2}')
+
+    # Check if the variable is less than 1
+    if (( $(echo "$swap_size < 1" | bc -l) )); then
+        echo "The Swap size is less than 1 bytes."
+    else
+        echo "The variable is not less than 1."
+        # Print the swap size
+        echo "Swap Size: $swap_size"
+        # disable swap permanently
+        comment_lines_with_keyword "swap" "/etc/fstab"
+    fi
+
+}
+
+echo "K8S required Disable Swap"
+read -p "Do you want to Disable Swap Permanently? Otherwise Disable Temporarily (yes/no): " answer
+
+case "$answer" in
+    [Yy][Ee][Ss]|[Yy])
+        echo "Disable Swap Permanently"
+        disable_swap_permanently
+        # apply the new swap setting
+        mount -a
+        ;;
+    [Nn][Oo]|[Nn])
+        echo "Disable Swap Temporarily"
+        sudo swapoff -a
+        ;;
+    *)
+        echo "Invalid input. Please enter 'yes' or 'no'."
+        ;;
+esac
