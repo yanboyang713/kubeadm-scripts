0da23a09e453229339a9763889ec0534e07d935e
Boyang Yan <yanboyang713@gmail.com>
2023-12-31 18:50:34 -0500

    update

--- scripts/common.sh
+++ scripts/common.sh
@@ -65,24 +65,8 @@ disable_swap(){
     esac
 }
 
-
-set -euxo pipefail
-
-# Variable Declaration
-
-KUBERNETES_VERSION="1.26.1-00"
-
-# disable swap
-disable_swap
-
-sudo apt-get update -y
-
-
-# Install CRI-O Runtime
-
-OS="xUbuntu_22.04"
-
-VERSION="$(echo ${KUBERNETES_VERSION} | grep -oE '[0-9]+\.[0-9]+')"
+# Enable iptables Bridged Traffic on all the Nodes
+enable_iptables_bridged_traffic(){
 
 # Create the .conf file to load the modules at bootup
 cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
@@ -90,6 +74,7 @@ overlay
 br_netfilter
 EOF
 
+# Execute the following commands to enable overlayFS & VxLan pod communication.
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
@@ -100,42 +85,128 @@ net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
+# Reload the parameters.
 sudo sysctl --system
 
+}
+
+# Install CRI-O Runtime
+install_CRI-O_runtime(){
+
+    dist_id=$(lsb_release -is)   # This gets the distributor ID (e.g., Ubuntu)
+    version=$(lsb_release -rs)   # This gets the release version of the OS
+
+    if [ "$dist_id" = "Ubuntu" ]; then
+	echo "Operating system is Ubuntu."
+
+	if [ "$version" = "22.04" ]; then
+            # Set variable if Ubuntu version is 22.04
+	    OS="xUbuntu_22.04"
+            echo "Ubuntu version is 22.04."
+	elif [ "$version" = "21.10" ]; then
+	    OS="xUbuntu_21.10"
+            echo "Ubuntu version is 21.10."
+	else
+            echo "Ubuntu version: $version, is not support"
+	fi
+    else
+	echo "Operating system is not Ubuntu. Detected: $dist_id"
+    fi
+
+    VERSION="$(echo $1 | grep -oE '[0-9]+\.[0-9]+')"
+
+    # Add CRI source and key
+    echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+    echo "deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+    sudo mkdir -p /usr/share/keyrings
+    curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | gpg --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
+    curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | gpg --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
+
+    # Update and install crio and crio-tools.
+    sudo apt-get update
+    sudo apt-get install cri-o cri-o-runc
+
 # Add CRI source and key
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
-EOF
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:"$VERSION".list
-deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
-EOF
+#cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+#deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+#EOF
+#cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:"$VERSION".list
+#deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+#EOF
 
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:"$VERSION"/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+# Add the gpg keys.
+#curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:"$VERSION"/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+#curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
 
-sudo apt-get update
-sudo apt-get install cri-o cri-o-runc -y
+# Update and install crio and crio-tools.
+#sudo apt-get update
+#sudo apt-get install cri-o cri-o-runc -y
 
-sudo systemctl daemon-reload
-sudo systemctl enable crio --now
+    # Reload the systemd configurations and enable cri-o.
+    sudo systemctl daemon-reload
+    sudo systemctl enable crio --now
 
-echo "CRI runtime installed susccessfully"
+    echo "CRI runtime installed susccessfully"
+}
 
 # Install kubelet, kubectl and Kubeadm
+install_kubelet_kubectl_kubeadm(){
+    # Install the required dependencies.
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl
+    sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+    # Add the GPG key and apt repository.
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
-sudo apt-get install -y jq
+    # Update apt and install the latest version of kubelet, kubeadm, and kubectl.
+    sudo apt-get update -y
+    sudo apt-get install -y kubelet=$1 kubectl=$1 kubeadm=$1
 
+    # Add hold to the packages to prevent upgrades.
+    sudo apt-mark hold kubelet kubeadm kubectl
 
-IFACE=$(ip route show to match default | perl -nle 'if ( /dev\s+(\S+)/ ) {print $1}')
-local_ip=$(ip --json a s | jq -r --arg IFACE "$IFACE" '.[] | if .ifname == $IFACE then .addr_info[] | if .family == "inet" then .local else empty end else empty end')
+}
 
-echo "$IFACE interface with IP: $local_ip"
+#Add the node IP to KUBELET_EXTRA_ARGS.
+set_node_IP(){
+    sudo apt-get update -y
+    sudo apt-get install -y jq
 
-printf "KUBELET_EXTRA_ARGS=--node-ip=%s\n" "$local_ip" | sudo tee -a /etc/default/kubelet
+    IFACE=$(ip route show to match default | perl -nle 'if ( /dev\s+(\S+)/ ) {print $1}')
+    local_ip=$(ip --json a s | jq -r --arg IFACE "$IFACE" '.[] | if .ifname == $IFACE then .addr_info[] | if .family == "inet" then .local else empty end else empty end')
+
+    echo "$IFACE interface with IP: $local_ip"
+
+    printf "KUBELET_EXTRA_ARGS=--node-ip=%s\n" "$local_ip" | sudo tee -a /etc/default/kubelet
+
+}
+
+set -euxo pipefail
+
+# Variable Declaration
+
+KUBERNETES_VERSION="1.26.1-00"
+
+main() {
+    # disable swap
+    disable_swap
+
+    sudo apt-get update -y
+
+    enable_iptables_bridged_traffic
+
+    # Install CRI-O Runtime
+    install_CRI-O_runtime $KUBERNETES_VERSION
+
+    # Install kubelet, kubectl and Kubeadm
+    install_kubelet_kubectl_kubeadm $KUBERNETES_VERSION
+
+    # Add the node IP to KUBELET_EXTRA_ARGS.
+    set_node_IP
+
+}
+
+# Call the main function
+main "$@"
